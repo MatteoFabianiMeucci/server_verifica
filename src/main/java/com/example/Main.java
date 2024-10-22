35e5f3b66baef4931a8550ddbc73817717ba7702
lai2-24 <lai2-24@itismeucci.com>
2024-10-22 13:07:05 +0200

    secondo commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,23 @@
 package com.example;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        System.out.println("Server avviato!");
+        ServerSocket s1 = new ServerSocket(3000);
+
+        Random random = new Random();
+        int numero = random.nextInt(100);
+
+        do{
+            Socket s = s1.accept();
+            System.out.println("un Client si è collegato");
+
+            MioThread t = new MioThread(s, numero);
+            t.start();
+        } while(true);
     }
 }
---
+++ src/main/java/com/example/MioThread.java
@@ -0,0 +1,60 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+import javax.print.DocFlavor.STRING;
+
+public class MioThread extends Thread{
+    Socket s;
+    int numero;
+    int contatore;
+
+    public MioThread(Socket s, int numero){
+        this.s = s;
+        this.numero = numero;
+        this.contatore = 0;
+    }
+
+    public void run(){
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()))) {
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+            String stringRicevuta;
+            String stringInviata;
+            boolean acceso = true;
+
+            do{
+                stringRicevuta = in.readLine();
+                try{
+                    int num = Integer.parseInt(stringRicevuta);
+
+                    if(num >= 0 || num<= 100){
+                        if (num < numero){
+                            out.writeBytes("<\n");
+                            contatore++;
+                        }
+
+                        if (num > numero) {
+                            out.writeBytes(">\n");
+                            contatore++;
+                        }
+
+                        if (num == numero) {
+                            out.writeBytes("=\n");
+                            out.writeBytes(contatore + "\n");
+                        }
+                    }
+                } catch (Exception e){
+                    out.writeBytes("!\n");
+                }
+            } while(acceso);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
